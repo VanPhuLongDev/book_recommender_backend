0ff4438b0909db8c7e9773a2e6107a1570263039
NguyenDucDuy <ducduynguyen347@gmail.com>
2024-06-03 03:57:10 +0700

    add hybrid recocommend

--- src/controller/recommend_controller.py
+++ src/controller/recommend_controller.py
@@ -43,7 +43,11 @@ def predictTopK():
         results = recommender(similar_user_id, topK)
     except Exception as e:
         print (e)
-    return results
+    return jsonify({
+                    "listBooks": results,
+                    "message": "SUCCEESS",
+                    "status": 1
+                }), 201
 
 
 @recommend.route('/predict-convmf', methods=['POST'])
@@ -53,12 +57,66 @@ def predictTopK_convmf():
     bookIds = data.get('book_ids')
     similar_user_id = find_similar_user(bookIds,model_knn,user_book_matrix)
     topK= data.get('topK')
+    print('top k :', topK)
     print(similar_user_id, topK)
-    recs,score_item = model.recommend(user_id="9", k=10)
-    print('recs controller: ',recs)
-    print(score_item)
-    return jsonify({
-                    "listBooks": recs,
-                    "message": "SUCCEESS",
-                    "status": 1
-                }), 201
+    try:
+        recs,score_item = model.recommend(user_id=str(similar_user_id), k=topK)
+        print('recs controller: ',recs)
+        return jsonify({
+                        "listBooks": recs,
+                        "message": "SUCCEESS",
+                        "status": 1
+                    }), 201
+    except Exception as e:
+        print('error in convmf recommend: ',e)
+        return None
+
+def combine_recommend(a, b):
+    # Tìm các phần tử chung
+    common_elements = list(set(a) & set(b))
+    
+    # Loại bỏ các phần tử chung khỏi hai mảng ban đầu
+    a_unique = [x for x in a if x not in common_elements]
+    b_unique = [x for x in b if x not in common_elements]
+    
+    # Tạo danh sách kết hợp từ các phần tử chung
+    result = common_elements[:]
+    
+    # Thêm các phần tử không chung xen kẽ nhau
+    len_a = len(a_unique)
+    len_b = len(b_unique)
+    max_len = max(len_a, len_b)
+    
+    for i in range(max_len):
+        if i < len_a:
+            result.append(a_unique[i])
+        if i < len_b:
+            result.append(b_unique[i])
+    
+    return result
+
+@recommend.route('/hybrid_recommend', methods=['POST'])
+def hybrid_recommend():
+    model = current_app.model
+    data = request.get_json()
+    bookIds = data.get('book_ids')
+    similar_user_id = find_similar_user(bookIds,model_knn,user_book_matrix)
+    topK= data.get('topK')
+    try:
+        lightcgn = recommender(similar_user_id, topK)
+        recs,score_item = model.recommend(user_id=str(similar_user_id), k=topK)
+        recs_int = [int(i) for i in recs]
+        result = combine_recommend(recs_int,lightcgn)
+        print('result: ',result)
+        value =  jsonify({
+                        "listBooks": result,
+                        "lightcgn": lightcgn,
+                        "convmf": recs_int,
+                        "score_convmf": score_item.tolist(),
+                        "message": "SUCCEESS",
+                        "status": 1
+                    }), 201
+        return value
+    except Exception as e:
+        print('error in hybrid recommend system: ', e)
+        return None
